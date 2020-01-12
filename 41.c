696c4e2ea35150f332eddbfc3a16c2ccc13cc272
JERUKA9 <jeruka9@gmail.com>
2020-01-12 12:31:27 +0100

    Add files via upload

---
+++ 41.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/* this is a c program*/
+
+int main ()
+{
+
+  printf("");
+
+  puts("c program");//simple output //
+
+  scanf ("%s");
+
+
+
+
+
+
+
+    return 0;
+
+}
