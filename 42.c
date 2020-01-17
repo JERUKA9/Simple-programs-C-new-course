c8f48d6a0b93605c000c3b45ecc3e36e2583f735
JERUKA9 <jeruka9@gmail.com>
2020-01-17 18:46:45 +0100

    Add files via upload

---
+++ 42.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
+
+int main ()
+{
+
+int an_age=21;
+
+float pi=3.14;
+
+char mychar='a';
+
+doble big_pi=3.14223232323232122;
+
+
+
+
+
+
+
+
+  printf("age is %d",an_age);
+
+
+
+
+
+
+
+    return 0;
+
+}
