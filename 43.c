02dfd8cc29e8bc28924b6621ee519f16336b0eca
JERUKA9 <jeruka9@gmail.com>
2020-01-18 09:53:18 +0100

    Add files via upload

---
+++ 43.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+    /**
+    +
+    -
+    *
+    /
+    %
+    **/
+
+    int a=23,b=45;
+    int c=8,d=2;
+
+    int sum;
+
+    sum=a*b;
+
+    int division;
+
+    division= a/b,c/d;
+
+
+
+
+    printf("division = %d",division);
+
+
+    int multiplication;
+
+    multiplication =a*b;
+
+
+
+  printf("product %d",multiplication);
+
+
+  printf("sum %d",sum);
+
+
+    return 0;
+
+}
