80147ac46032be522e3553f1a9527bbf3ddfe8bf
JERUKA9 <jeruka9@gmail.com>
2020-01-22 20:11:50 +0100

    Add files via upload

---
+++ 47.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+    int age;
+
+    printf("enter age");
+
+    scanf("%d",&age);
+
+
+    if (age>18)
+
+        printf("the age is greater than 18");
+
+        else
+
+            printf("age is less than 18");
+
+            if (age == 18)
+
+printf("age is equal to 18");
+
+
+
+
+    return 0;
+
+}
