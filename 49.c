52d2cd96ae2e51f66f38a365e9546998d195e457
JERUKA9 <jeruka9@gmail.com>
2020-01-24 18:55:00 +0100

    Add files via upload

---
+++ 49.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+
+    int marks;
+
+switch(marks)
+
+case 95:
+
+    printf("Super");
+
+    break;
+
+
+case 90:
+
+
+printf("Excelent");
+
+break;
+
+case 60:
+
+    printf("good");
+
+    break;
+
+
+case 75:
+    printf("Very Good");
+
+case 40:
+
+    printf("OK");
+
+
+break;
+
+default:
+
+
+
+printf("Grade  unavailble");
+
+
+
+
+    return 0;
+
+}
