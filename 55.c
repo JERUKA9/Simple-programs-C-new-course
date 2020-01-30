95494ef959bbfded930351587128390b66aad065
JERUKA9 <jeruka9@gmail.com>
2020-01-30 19:47:03 +0100

    Add files via upload

---
+++ 55.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void myfunctionname()
+{
+
+printf("sum %d",10+52);
+
+  printf("inside a function");
+
+
+}
+int main ()
+{
+
+myfunctionname();
+
+
+
+return 0;
+}
+
