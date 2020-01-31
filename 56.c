9ee79dbd3b22e8ca9e91fd2bdaf53daa4b315143
JERUKA9 <jeruka9@gmail.com>
2020-01-31 21:51:41 +0100

    Add files via upload

---
+++ 56.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void sum(int a,int b)
+{
+ int  sum =a+b;
+
+ printf("sum of two numbers %d",sum);
+
+
+}
+
+int main ()
+{
+
+
+sum(50,60);
+
+
+return 0;
+}
+
