817ead5d5a57713d08b30ddebab9ddae45144e1c
JERUKA9 <jeruka9@gmail.com>
2020-02-03 21:31:24 +0100

    Add files via upload

---
+++ 58.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int g;
+
+
+void numberprint(){
+
+
+printf("global %d",g);
+
+}
+
+{
+
+int x;
+
+int a;
+
+}
+int main ()
+{
+
+int a =10;
+
+printf("local %d",a);
+
+
+
+
+return 0;
+
+
+}
+
