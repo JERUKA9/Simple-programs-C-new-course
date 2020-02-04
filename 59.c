e3a75554704573c9f2ed248da2b2bb264c817b38
JERUKA9 <jeruka9@gmail.com>
2020-02-04 08:52:31 +0100

    Add files via upload

---
+++ 59.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main ()
+{
+
+
+int mynumberarray[3] ={0,1,2};
+
+int accesarray=mynumberarray[1];
+
+printf("value %d",accesarray);
+
+
+
+
+
+
+
+return 0;
+
+
+}
+
