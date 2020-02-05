40226ccbd78c85b5c9105dd796b68c822545040f
JERUKA9 <jeruka9@gmail.com>
2020-02-05 10:42:25 +0100

    Add files via upload

---
+++ 60.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main ()
+{
+int mynumberarray[2][3]={1,2,3},
+                        {5,6,8};
+
+
+                        for (int i=0;i<2;i++)
+
+
+                        for (int j=0;i<3;j++)
+
+                        printf("array [%d][%d]",i,j,mynumberarray);
+
+
+
+
+
+
+
+return 0;
+
+
+}
+
