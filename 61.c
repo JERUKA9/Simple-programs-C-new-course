dad7e10df3d4551f15cfa694bc1d8bbfd0b30073
JERUKA9 <jeruka9@gmail.com>
2020-02-07 10:31:36 +0100

    Add files via upload

---
+++ 61.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int arraysum(int myarray[],int size);
+{
+
+  int sum = 0;
+
+  for(i=0;i<6;i++)
+   {
+
+    sum+=myarray[i];
+
+
+   }
+
+   return sum;
+
+}
+
+
+int main ()
+{
+
+
+int mynumber array[6]={0,1,2,3,4,5};
+
+
+int sum_of_array = arraysum(mynumber array,6);
+
+
+printf("array sum %d",mynumber array);
+
+return 0;
+
+
+}
+
