9864d52a9da77d30bf52f6ac4e9bc844ec1e9261
JERUKA9 <jeruka9@gmail.com>
2020-02-08 07:56:26 +0100

    Add files via upload

---
+++ 62.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main ()
+{
+
+int value= 30;
+
+int *pointer_p;
+
+pointer_p = &value;
+
+
+printf("value adress is %p",&value);
+
+printf("value is %x",&value);
+
+printf("value of pointer %d",*pointer_p);
+
+
+
+
+
+return 0;
+
+
+}
+
