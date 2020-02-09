e15f83057c273771af18bb8acecb84e6e1d1daeb
JERUKA9 <jeruka9@gmail.com>
2020-02-09 08:13:23 +0100

    Add files via upload

---
+++ 63.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+int main ()
+{
+
+ int val_array[3] ={30,40,50};
+
+ int *pointer_array[3];
+
+ for (int i =0 ; i<3;i++){
+
+ pointer_array[i]= &val_array[i];
+
+ printf("value of array %d",pointer_array[i]);
+
+
+ }
+
+return 0;
+
+
+}
+
