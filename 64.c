fbc98caa8f8de188cb7847c15c5ca846bbc1f59d
JERUKA9 <jeruka9@gmail.com>
2020-02-10 09:31:51 +0100

    Add files via upload

---
+++ 64.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void  getvalue(int *my_pointer)
+{
+
+*my_pointer = 10000;
+
+return;
+
+
+
+}
+
+
+
+int main ()
+{
+ int get_the_value;
+
+ getvalue(&get_the_value);
+
+ printf("value %d",get_the_value);
+
+
+
+
+return 0;
+
+
+}
+
