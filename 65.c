ca9f1d56776f972662bb00737b9e097b1755e22f
JERUKA9 <jeruka9@gmail.com>
2020-02-11 13:31:35 +0100

    Add files via upload

---
+++ 65.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int getsum(int *array_val,int size)
+{
+
+int sum =0;
+
+for(int i;i<size;i++)
+{
+
+  sum+=array_val[i];
+
+}
+  return sum;
+
+}
+
+
+
+
+int main ()
+{
+
+int my_array[4]={13,20,30,40};
+
+
+
+ int  mysum = getsum(my_array,4);
+
+printf("the value of sum is %d",mysum);
+
+
+
+return 0;
+
+
+}
+
