049603c829d5119b23b009fb858a169a27b88ac8
JERUKA9 <jeruka9@gmail.com>
2020-02-12 07:18:17 +0100

    Add files via upload

---
+++ 66.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+
+
+
+
+int main ()
+{
+
+ char my_string[6]={'h','e','l','l','o','\0'};
+
+ char my_other_string []=|"hello";
+
+printf("my string value =%s",my_string);
+
+printf("my other string value =%s",my_other_string);
+
+
+return 0;
+
+
+}
+
