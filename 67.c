041b65f2041f00586103b812c81d0ad938bd03a8
JERUKA9 <jeruka9@gmail.com>
2020-02-13 22:08:09 +0100

    Add files via upload

---
+++ 67.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+
+
+
+
+int main ()
+{
+
+
+ char string1 []="hello";
+ char string2 []="world";
+ char string3 []="C";
+
+
+
+
+strcpy(string1,string3);
+
+strcat(string1,string2);
+
+
+int length_string1 = strlen(string1);
+
+printf("strcpy %s",string3);
+
+printf("strcat %s",string2);
+
+printf("strlen %s",string2);
+
+
+return 0;
+
+
+}
+
