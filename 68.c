e6c5273ea8df8c5f178430bfeab256306c5b04df
JERUKA9 <jeruka9@gmail.com>
2020-02-14 10:17:31 +0100

    Add files via upload

---
+++ 68.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct student{
+
+int id;
+
+   char name[20];
+
+   float percentage;
+
+
+
+
+};
+
+
+
+
+int main ()
+{
+
+
+struct student record1;
+
+record1.id =1;
+
+strcpy(record1.name,"John");
+
+record1.percentage=70.20;
+
+
+printf("id=%d  name=%s   percentage =%f",record1.id,record.name,record.percentage);
+
+
+
+
+return 0;
+
+
+}
+
