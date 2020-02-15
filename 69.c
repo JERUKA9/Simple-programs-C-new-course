dde9097b6a1a073774428496e3c75204c89c367d
JERUKA9 <jeruka9@gmail.com>
2020-02-15 08:52:50 +0100

    Add files via upload

---
+++ 69.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+union myunion {
+
+int myint;
+
+char mychar;
+
+
+}myunion;
+
+
+
+
+
+
+
+int main ()
+{
+
+
+myunion.myint =4;
+
+myunion.mychar =9;
+
+printf("%i /n",myunion.mychar);
+
+
+
+return 0;
+
+
+}
+
